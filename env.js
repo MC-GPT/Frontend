d903fce5cfee803ff0aaef47faa1751527b12cd3
orangejoam <qwt629@gmail.com>
2023-10-01 15:46:55 +0900

    firebase

---
+++ env.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyAba6l_xFtJwtXxBeldzWbStiWQaKvmcUs',
+  authDomain: 'mc-nugu.firebaseapp.com',
+  projectId: 'mc-nugu',
+  storageBucket: 'mc-nugu.appspot.com',
+  messagingSenderId: '924699713647',
+  appId: '1:924699713647:web:53eb01f767b29fd9ae6d9a',
+  measurementId: 'G-H5PPY4C03P',
+};
